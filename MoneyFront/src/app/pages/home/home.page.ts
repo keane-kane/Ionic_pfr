3c13d941bbda64e641f2f092e899315e54e03282
aziz <abdouazizk50@gmail.com>
2021-03-02 13:57:10 +0000

    integration of the home page

---
+++ MoneyFront/src/app/pages/home/home.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController, LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit  {
+
+  constructor(
+    private router: Router,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+  ) {
+
+  }
+ ngOnInit(){
+   setInterval(() => {
+    console.log('Hello world');
+    this.router.navigate(['/login']);
+  } ,  2000);
+ }
+  async presentToast() {
+    const toast = await this.toastCtrl.create({
+      message: 'Bienvenu dans Save Money SA',
+      duration: 2000,
+      position: 'middle',
+      cssClass: 'toast-costum-class',
+    });
+    toast.present();
+  }
+
+
+}
